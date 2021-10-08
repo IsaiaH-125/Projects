d631dd4b297b4242f25298eb87e34728865fa5c9
IsaiaH-125 <isaiahcc@icloud.com>
2021-10-08 12:50:54 -0700

    Second commit grayscale image

--- image-magic.py
+++ image-magic.py
@@ -6,31 +6,40 @@ from PIL import Image
 # Load the image (pumpkin)
 
 image = Image.open('./halloween-unsplash.jpg')
+output_image = Image.open('./halloween-unsplash.jpg')
 
-
-# Grab pixel info
-
-a_pixel = image.getpixel((1, 3))
-
-print(a_pixel)
-
-
-# Iterate over EVERY PIXEL
 # Get dimensions (size) of the image
 
-image_width = image.width
-image_height = image.height
 
-for y in range(image_height):
-    # Left to right
-    for x in range(image_width):
-        # Grab pixel information for THIS pixel
-        pixel = image.getpixel((x, y))
 
-        print(f"\nPixel Location: {x} {y}")
-        # Print pixel values
-        print(f"red: {pixel[0]}")
-        print(f"green {pixel[1]}")
-        print(f"blue {pixel[2]}")
+# Grab r, g, b,
+
+pixel = (red, green, blue)
+
+red = pixel[0]
+green = pixel[1]
+blue = pixel[2]
+
+red, green, blue = pixel
+
+average = int((red + green + blue) / 3)
+
+gray_pixel = (average, average, average)
+
+output_image.save('grayscale.jpg')
+
+# Put that in the new image
+
+output_image.putpixel((x, y), gray_pixel)
+# Calculate the average
+# Create a gray pixel
+# TODO: put that in the new image
+
+
+
+
+# Modify the image to convert it from color to grayscale
+# (r , g , b) ---> ( ?, ? , ?)
+
 
 
