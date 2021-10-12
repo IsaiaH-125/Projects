528d2acc5d3350477e31d9bdbcc12df8106316e3
IsaiaH-125 <isaiahcc@icloud.com>
2021-10-12 08:40:03 -0700

    Added Grayscale image

--- image-magic.py
+++ image-magic.py
@@ -1,45 +1,47 @@
+
 # Image Magic
 # Load an image and manipulate the pixels
 
 from PIL import Image
 
-# Load the image (pumpkin)
 
+# Load the image (pumpkin)
+# Open an output image that's the same size
 image = Image.open('./halloween-unsplash.jpg')
 output_image = Image.open('./halloween-unsplash.jpg')
 
+# Grab pixel information
+a_pixel = image.getpixel((0, 0))  # grab pixel (0, 0) top-left
+
+print(a_pixel)
+
+# Iterate over EVERY PIXEL
 # Get dimensions (size) of the image
+image_width = image.width
+image_height = image.height
 
+# Modify the image to convert it from colour to grayscale
+# ( r, g, b ) --> ( ?, ?, ? )
+# take the r g b, add them up and divide by 3
+# replace r, g, b with that AVERAGE value
 
+# Top to bottom
+for y in range(image_height):
+    # Left to right
+    for x in range(image_width):
+        # Grab pixel information for THIS pixel
+        pixel = image.getpixel((x, y))
 
-# Grab r, g, b,
+        # grab r, g, b
+        red, green, blue = pixel
 
-pixel = (red, green, blue)
+        # calculate the average
+        average = int((red + green + blue) / 3)
 
-red = pixel[0]
-green = pixel[1]
-blue = pixel[2]
+        # create a gray pixel
+        gray_pixel = (average, average, average)
 
-red, green, blue = pixel
-
-average = int((red + green + blue) / 3)
-
-gray_pixel = (average, average, average)
+        # put that in the new image
+        output_image.putpixel((x, y), gray_pixel)
 
 output_image.save('grayscale.jpg')
-
-# Put that in the new image
-
-output_image.putpixel((x, y), gray_pixel)
-# Calculate the average
-# Create a gray pixel
-# TODO: put that in the new image
-
-
-
-
-# Modify the image to convert it from color to grayscale
-# (r , g , b) ---> ( ?, ? , ?)
-
-
-
