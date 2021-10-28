eecb988136ca660afe086e7b35a4e570c7400dc8
IsaiaH-125 <isaiahcc@icloud.com>
2021-10-28 11:49:12 -0700

    Added typewriter effect to introduction code

--- midnight_rider.py
+++ midnight_rider.py
@@ -1,6 +1,10 @@
 # Midnight rider
 
+import sys
+import textwrap
+import time
 import midnight_rider_text
+
 
 # A text based game of intrigue and illusion
 
@@ -8,12 +12,23 @@ class Game:
     """Represent our game engine
 
     """
+
     def introduction(self):
         """Print the introduction text"""
-        print(midnight_rider_text.INTRODUCTION)
+        self.typewriter_effect(midnight_rider_text.INTRODUCTION)
+
+    def typewriter_effect(self, text: str) -> None:
+        """Print out to console with a typerwriter effect."""
+        for char in textwrap.dedent(text):
+            time.sleep(0.05)
+            sys.stdout.write(char)
+            sys.stdout.flush()
+
+
 def main() -> None:
-    game = Game() # starting a new game
+    game = Game()  # starting a new game
     game.introduction()
+
 
 if __name__ == "__main__":
     main()
