2042b945172f8c72f66810acb34c89ed45387c48
IsaiaH-125 <isaiahcc@icloud.com>
2021-10-29 12:01:25 -0700

    Implemented status check and QoL updates

--- midnight_rider.py
+++ midnight_rider.py
@@ -1,4 +1,4 @@
-# Midnight rider
+# Midnight Rider
 
 import sys
 import textwrap
@@ -6,40 +6,56 @@ import time
 import midnight_rider_text
 
 
-# A text based game of intrigue and illusion
+# A text-based game of intrigue and illusion
+
 
 class Game:
     """Represent our game engine
     Attribute:
-         Done: describes if the game is finished or not - bool
+        done: describes if the game is
+            finished or not - bool
+        distance_traveled: describe the distance that we have traveled so far this game, in km
+        amount of tofu: how much tofu we have left in our inventory
+        agents_distance: describes the distance between the player and the agents
     """
-    def __int__(self):
-        done = False
 
-    def introduction(self):
+    def __init__(self):
+        self.done = False
+        self.distance_traveled = 0
+        self.amount_tofu = 3
+        self.agents_distance = -20
+
+    def introduction(self) -> None:
         """Print the introduction text"""
         self.typewriter_effect(midnight_rider_text.INTRODUCTION)
 
     def typewriter_effect(self, text: str) -> None:
-        """Print out to console with a typerwriter effect."""
+        """Print out to console with a typewriter effect."""
         for char in textwrap.dedent(text):
             time.sleep(0.05)
             sys.stdout.write(char)
             sys.stdout.flush()
 
     def show_choices(self) -> None:
-        """show the user their choices"""
+        """Show the user their choices"""
         time.sleep(1)
         print(midnight_rider_text.CHOICES)
         time.sleep(1)
 
-    def get_choice(self): -> None:
-        """Get's the user's choice and changes the environment"""
+    def get_choice(self) -> None:
+        """Gets the user's choice and changes
+        the environment"""
         # Get the user's response
         user_choice = input().strip(",.?!").lower()
 
-        # Based on their choice, change the attributes of the class
-        if user_choice == "q":
+        # Based on their choice, change the attributes
+        # of the class
+        if user_choice == "e":
+            print("---Current Status---")
+            print(f"Distance Traveled: {self.distance_traveled}")
+            print(f"Tofu pieces remaining: {self.amount_tofu}")
+            print(f"Agents distance: {abs(self.agents_distance)} kilometers behind")
+        elif user_choice == "q":
             self.done = True
 
 
@@ -47,12 +63,10 @@ def main() -> None:
     game = Game()  # starting a new game
     game.introduction()
 
-    # Main Loop:
     while not game.done:
+        game.show_choices()
+        game.get_choice()
 
-         #
-         game.show_choices()
-         #
 
-    if __name__ == "__main__":
-        main()
+if __name__ == "__main__":
+    main()
--- midnight_rider_text.py
+++ midnight_rider_text.py
@@ -1,25 +1,18 @@
 # Constants
 
 INTRODUCTION = """Welcome to Midnight Rider
-
 WE'VE STOLEN A CAR. WE NEED TO GET IT HOME.
 THE CAR IS SPECIAL.
-
 THAT'S WHY THE GOVERNMENT WANTS IT.
-
 WE CAN'T LET THEM HAVE IT.
-
-ONE GOAL: SURVIVAL..... AND THE CAR
+ONE GOAL: SURVIVAL... AND THE CAR
 REACH THE SAFEHOUSE BEFORE THE MAN
 GON GETCHU.
 """
 
 CHOICES = """
-    --- Your choices ---
-    Q. Quit the game.
+    --- Your Choices ---
+    E. Current Status
     ---
-    
-
-
-
+    Q. Quit the game.
 """
