c71502027375ddf2e3576c08b8f5df93eb8d8028
IsaiaH-125 <isaiahcc@icloud.com>
2021-11-05 11:40:25 -0700

    Added endgame, and changes in midnight_rider_text.py

--- midnight_rider_text.py
+++ midnight_rider_text.py
@@ -12,7 +12,66 @@ GON GETCHU.
 
 CHOICES = """
     --- Your Choices ---
-    E. Current Status
+    A. Eat a tofu
+    B. Travel moderately
+    C. Speed ahead at full speed
+    D. Stop to refuel
+    E. Check current status
+    Q. Quit the game
     ---
-    Q. Quit the game.
 """
+
+REFUEL = """-------You filled the fuel tank.
+-------The agents got closer...
+"""
+
+EAT_TOFU = """-------Mmmmm. Soybean goodness.
+-------Your hunger is sated.
+"""
+
+NO_TOFU = """-------You have no tofu left. 😥
+"""
+
+SEVERE_HUNGER = """\n*******Your stomach rumbles.
+*******You need to eat something quick.
+"""
+
+HUNGER = """\n*******Your hunger is small but manageable.
+"""
+
+ENDGAME_TEXT = [
+    """\n\nThe game has ended. Thanks for playing!""",     # Quit the game
+    """\n\nTHE AGENTS HAVE CLOSED IN ON YOU.
+THERE ARE AT LEAST 20 CARS SURROUNDING YOU.
+THE LEAD CAR BUMPS YOUR PASSENGER SIDE.
+YOU MANAGE TO CORRECT YOUR STEERING
+TO KEEP YOU FROM CRASHING.
+YOU DIDN'T SEE THE AGENT'S CAR RIGHT BESIDE YOU.
+THAT DRIVER BUMPS YOUR CAR.
+AND THAT'S IT.
+YOU SPIN UNCONTROLLABLY.
+YOUR CAR FLIPS OVER AT LEAST TWO TIMES.
+OR MORE... YOU SEEM TO HAVE LOST COUNT.
+SIRENS.
+"ARE THEY ALIVE?" THEY SAY AS YOU HEAR
+FOOTSTEPS COMING CLOSER.
+"DOESN'T MATTER, ALL WE WANTED WAS THE CAR."
+YOU SEE A DOG SLOWLY STEP OUT OF THE
+OVERTURNED CAR.
+"YOU WILL NEVER STOP THE REVOLUTION!"
+THE DOG SEEMS TO SAY TO THE AGENTS.
+IT WAS IN THE CAR THE WHOLE TIME.
+YOU DRIFT OFF INTO UNCONSCIOUSNESS...
+---GAME OVER---\n""",     # LOSE - Agents have caught up
+    """\n\nYOUR CAR SPUTTERS AND SEEMS TO LET OUT
+A BIG SIGH. THERE'S NO MORE FUEL LEFT.
+THE AGENTS SURROUND YOU AND STEP OUT
+OF THEIR CARS. THE LEAD AGENT RIPS
+THE DOOR OPEN AND THROWS YOU OUT OF
+YOUR CAR.
+"WE FINALLY GOT IT."
+YOU FAILED.
+----GAME OVER----""",     # LOSE - Run out of fuel/charge
+    """""",
+    """""",
+]
