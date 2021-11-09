b38b0de4b7f254bbbb87133458ab097751b12a77
IsaiaH-125 <isaiahcc@icloud.com>
2021-11-09 09:50:11 -0800

    Made a drawing

--- pygame_drawing.py
+++ pygame_drawing.py
@@ -21,9 +21,45 @@ GREY  = (128, 128, 128)
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
+WINDOW_TITLE = "Pygame Drawing"
 
 def main() -> None:
     """Driver of the python script"""
+    # Create the screen
+    screen = pygame.display.set_mode(SCREEN_SIZE)
+    pygame.display.set_caption("WINDOW_TITLE")
+
+
+
+    # Create some local variables that describe the environment
+    done = False
+    clock = pygame.time.Clock()
+
+
+    # ---------------- Main loop
+    while not done:
+    # Create the main loop
+        # --------------- listener
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+        # ------------ environment
+        # ------------ DRAW THE ENVIRONMENT
+        screen.fill(WHITE)          #fill with bgcolor
+        pygame.draw.rect(screen, RED, [100, 100, 75, 30])
+
+        pygame.draw.circle(screen, BLACK, (500, 200), 60)
+        pygame.draw.circle(screen, BLACK, (700, 200), 60)
+        pygame.draw.circle(screen, GREY, (500, 200), 55)
+        pygame.draw.circle(screen, GREY, (700, 200), 55)
+        pygame.draw.circle(screen, BLACK, (600, 255), 75)
+        pygame.draw.circle(screen, GREY, (600, 255), 65)
+        pygame.draw.circle(screen, WHITE, (580, 200), 10)
+
+        # Update the screen
+        pygame.display.flip()
+        # Tick the clock
+        clock.tick(60)
 if __name__ == '__main__':
     main()
 
