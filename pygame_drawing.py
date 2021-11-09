8a7cdbfd7fb782b252c043b71eb314602d0eca03
IsaiaH-125 <isaiahcc@icloud.com>
2021-11-09 08:47:26 -0800

    Added Pygame_drawing and completed boilerplate

---
+++ pygame_drawing.py
@@ -0,0 +1,29 @@
+# Pygame_Drawing
+# Author: isaiah
+# Date: Nov 9/21
+
+# Get introduced to Pygame and draw objects on screen
+
+import pygame
+
+pygame.init()
+
+# COLOURS - (R, G, B)
+# CONSTANTS ALL HAVE CAPS FOR THEIR NAMES
+WHITE = (255, 255, 255)
+BLACK = (  0,   0,   0)
+RED   = (255,   0,   0)
+GREEN = (  0, 255,   0)
+BLUE  = (  0,   0, 255)
+GREY  = (128, 128, 128)
+
+# CONSTANTS
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
+
+def main() -> None:
+    """Driver of the python script"""
+if __name__ == '__main__':
+    main()
+
